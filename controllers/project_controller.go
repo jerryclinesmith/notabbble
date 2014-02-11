3f607ce4b4f5b61e74df43581ea65510a2c42520
Jerry Clinesmith <jerryclinesmith@gmail.com>
2014-02-10 22:03:25 -0600

    Use http constants for status codes

--- controllers/project_controller.go
+++ controllers/project_controller.go
@@ -5,40 +5,41 @@ import (
 	"github.com/codegangsta/martini-contrib/render"
 	"github.com/jerryclinesmith/notabbble/models"
 	"github.com/jinzhu/gorm"
+	"net/http"
 	"time"
 )
 
 func ProjectIndex(db gorm.DB, r render.Render) {
 	projects := []models.Project{}
 	db.Find(&projects)
-	r.JSON(200, projects)
+	r.JSON(http.StatusOK, projects)
 }
 
 func ProjectGet(db gorm.DB, r render.Render, params martini.Params) {
 	var project models.Project
 	if err := db.First(&project, params["id"]).Error; err != nil {
-		r.JSON(404, map[string]interface{}{"error": "Project not found"})
+		r.JSON(http.StatusNotFound, map[string]interface{}{"error": "Project not found"})
 		return
 	}
-	r.JSON(200, project)
+	r.JSON(http.StatusOK, project)
 }
 
 func ProjectNew(r render.Render) {
 	project := new(models.Project)
-	r.JSON(200, project)
+	r.JSON(http.StatusOK, project)
 }
 
 func ProjectCreate(db gorm.DB, r render.Render, project models.Project) {
 	project.CreatedAt = time.Now()
 	project.UpdatedAt = time.Now()
 	db.Save(&project)
-	r.JSON(201, project)
+	r.JSON(http.StatusCreated, project)
 }
 
 func ProjectUpdate(db gorm.DB, r render.Render, params martini.Params, updatedProject models.Project) {
 	var project models.Project
 	if err := db.First(&project, params["id"]).Error; err != nil {
-		r.JSON(404, map[string]interface{}{"error": "Project not found"})
+		r.JSON(http.StatusNotFound, map[string]interface{}{"error": "Project not found"})
 		return
 	}
 
@@ -46,15 +47,15 @@ func ProjectUpdate(db gorm.DB, r render.Render, params martini.Params, updatedPr
 	project.UpdatedAt = time.Now()
 
 	db.Save(&project)
-	r.JSON(200, project)
+	r.JSON(http.StatusOK, project)
 }
 
 func ProjectDelete(db gorm.DB, r render.Render, params martini.Params) {
 	var project models.Project
 	if err := db.First(&project, params["id"]).Error; err != nil {
-		r.JSON(404, map[string]interface{}{"error": "Project not found"})
+		r.JSON(http.StatusNotFound, map[string]interface{}{"error": "Project not found"})
 		return
 	}
 	db.Delete(project)
-	r.JSON(204, nil)
+	r.JSON(http.StatusNoContent, nil)
 }
